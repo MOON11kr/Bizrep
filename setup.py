49141cf123abe47d67a2dac40febe47538e3fb8b
MOON11kr <167286067+MOON11kr@users.noreply.github.com>
2025-03-21 16:56:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="A Python library for predictive analytics and business forecasting.", 
     long_description=open("README.md").read(), 
     long_description_content_type="text/markdown",  
-    url="https://github.com/MOON11kr/BizML",
+    url="https://github.com/MOON11kr/Bizrep",
     packages=find_packages(), 
     install_requires=[  # List of dependencies
         "pandas",
